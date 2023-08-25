389563b280e86da5f64d63fdac5a2b75dd818913
tlloydthwaites <91047784+tlloydthwaites@users.noreply.github.com>
2023-08-25 12:16:37 +1000

    Update role_client.rb

--- lib/keycloak-admin/client/role_client.rb
+++ lib/keycloak-admin/client/role_client.rb
@@ -20,6 +20,19 @@ module KeycloakAdmin
       RoleRepresentation.from_hash JSON.parse(response)
     end
 
+    def users(role_name, first=0, max=100)
+      url = "#{role_name_url(role_name)}/users"
+      query = {first: first.try(:to_i), max: max.try(:to_i)}.compact
+      unless query.empty?
+        query_string = query.to_a.map { |e| "#{e[0]}=#{e[1]}" }.join("&")
+        url = "#{url}?#{query_string}"
+      end
+      response = execute_http do
+        RestClient::Resource.new(url, @configuration.rest_client_options).get(headers)
+      end
+      JSON.parse(response).map { |user_as_hash| UserRepresentation.from_hash(user_as_hash) }
+    end
+  
     def save(role_representation)
       execute_http do
         RestClient::Resource.new(roles_url, @configuration.rest_client_options).post(
